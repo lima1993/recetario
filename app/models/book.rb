ed5aadc5dbeee3d2a1f5e59f27f11004d3e69b11
lima1993 <trdnisnak@gmail.com>
2015-10-21 23:35:52 -0500

    actu

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,6 @@ class Book < ActiveRecord::Base
 	belongs_to :category
 	has_many :reviews
 
-	has_attached_file :book_img, :styles => { :book_index => "250x350>", :book_show => "325x475>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :book_img, styles:  { medium: "250x350>", thumb:"325x475>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :book_img, :content_type => /\Aimage\/.*\Z/
 end
